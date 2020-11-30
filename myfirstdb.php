ba6f6cab4f5aa7aa24674d3fed453bc16049e838
Aung Min Thant <aungminthant221@gmail.com>
2020-11-30 14:02:52 +0630

    Lessons

--- myfirstdb.php
+++ myfirstdb.php
@@ -10,13 +10,56 @@ define("DB_PASS","");
 	//mysqli_connect(host,username,password,dbname);
 function dbconnection(){
 	$db=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-	echo mysqli_connect_errno()>0 ? "Connection Error":"Connection Success";
+	//echo mysqli_connect_errno()>0 ? "Connection Error":"Connection Success";
+	if(mysqli_connect_errno()>0){
+		//echo "Connectin Error";
+		die("Connectin Failes");
+	}else {
+	     return $db;
 }
 
-beautyprint(dbconnection());
+}
+
+//beautyprint(dbconnection());
+
+//2 Query ( Table )
+$query = "SELECT * FROM users";
+
+function executequery($qry){
+
+//1 Database ( Successful )
+$database = dbconnection();
+
+//3 Connect to Database and Query
+// mysqli_query("database","query");
+$results = mysqli_query($database,$qry);
+
+if(mysqli_num_rows($results) > 0){
+	foreach($results as $result){
+		foreach($result as $key=>$value){
+			echo "Key is = ${key} " . "Value is = ${value}"."<br/>";
+		}
+	}
+}else {
+	echo "No Data";
+}
+
+
+}
+
+beautyprint(executequery($query));
+
+
+
+
+
+
+
+
+
 
 function beautyprint($array){
-
+	echo"<pre>".print_r($array,true)."</pre>";
 }
 
 ?>
